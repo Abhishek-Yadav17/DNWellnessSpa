04c9ef05f45ee66fab94364040716480bd8bf171
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-08-02 21:47:11 +0530

    membership section added

--- spa/src/components/Services.jsx
+++ spa/src/components/Services.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import '../styles/Services.scss';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { gsap } from 'gsap';
 
 const Services = () => {
+
+  const navigate = useNavigate();
 
   const cardData = [
     {
@@ -28,6 +30,27 @@ const Services = () => {
       desc: "A soothing, sensory massage using essential oils to restore emotional and physical well-being.",
       duration: "60-90mins",
       price: "₹ 2500 - ₹ 3000"
+    },
+    {
+      img: "/card4.jpg",
+      title: "Foot Reflexology",
+      desc: "A calming therapy using pressure points on the feet to restore balance and wellbeing.",
+      duration: "60-90mins",
+      price: "₹ 1500 - ₹ 2000"
+    },
+    {
+      img: "/card5.jpg",
+      title: "Bamboo Massage Therapy",
+      desc: "A deep tissue massage with warm bamboo sticks to ease tension and improve circulation.",
+      duration: "60mins",
+      price: "₹ 2500"
+    },
+    {
+      img: "/card6.jpeg",
+      title: "Sports Massage Therapy",
+      desc: "A targeted massage for active bodies to boost recovery, flexibility, and prevent injuries.",
+      duration: "60mins",
+      price: "₹ 2500"
     }
   ];
 
@@ -112,12 +135,12 @@ const Services = () => {
         <div className="services-hero" ref={contactRef}>
           <h1>Services</h1>
           <video src="/services.mp4" autoPlay loop muted></video>
-          <h2>Discover personalized therapies designed for your well-being.</h2>
+          <h2>Discover personalized spa therapies thoughtfully designed to support your health, balance, and overall well-being.</h2>
           <i class="ri-arrow-down-line"></i>
         </div>
         <div className="services">
           <h1>Our Signature Therapies</h1>
-          <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, at!</h4>
+          <h4>Enjoy soothing treatments that relax your body, calm your mind, and restore your inner balance.</h4>
           <div className="services-cards">
             {cardData.map((card, idx) => (
               <div className="card" key={idx}>
@@ -147,6 +170,42 @@ const Services = () => {
             </div>
             <div className="plans-right">
               <img ref={imgRef} src={currentPlan.img} alt="plans" />
+            </div>
+          </div>
+        </div>
+        <div className="memberships">
+          <h1>Memberships</h1>
+          <h4>Select the membership that best fits your wellness journey and enjoy exclusive benefits tailored to your needs.</h4>
+          <div className="membership-cards">
+            <div className="membership-card">
+              <h2>Silver Membership</h2>
+              <img src="/membership1.png" alt="membership" />
+              <h4>Validity 6 months</h4>
+              <ul>
+                <li><i class="ri-check-fill"></i><span>60</span> min sessions</li>
+                <li><i class="ri-check-fill"></i><span>16</span> sessions</li>
+              </ul>
+              <button onClick={() => navigate('/contact')}>Book now</button>
+            </div>
+            <div className="membership-card middle">
+              <h2>Diamond Membership</h2>
+              <img src="/membership2.png" alt="membership" />
+              <h4>Validity 1 year</h4>
+              <ul>
+                <li><i class="ri-check-fill"></i><span>60</span> min sessions</li>
+                <li><i class="ri-check-fill"></i><span>16</span> sessions</li>
+              </ul>
+              <button onClick={() => navigate('/contact')}>Book now</button>
+            </div>
+            <div className="membership-card">
+              <h2>Gold Membership</h2>
+              <img src="/membership3.png" alt="membership" />
+              <h4>Validity 6 months</h4>
+              <ul>
+                <li><i class="ri-check-fill"></i><span>60</span> min sessions</li>
+                <li><i class="ri-check-fill"></i><span>16</span> sessions</li>
+              </ul>
+              <button onClick={() => navigate('/contact')}>Book now</button>
             </div>
           </div>
         </div>
